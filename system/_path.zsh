bcbad327c96532e0e71baf51c42cd95ead04fb9f
Dan Ubilla <dan.ubilla@gmail.com>
2015-03-08 20:23:36 -0400

    Fix up zsh to load correctly

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,11 +1,11 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
+# export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
-# [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
-# export PATH="/usr/local/bin:$PATH"
-# export PATH="$PATH:$HOME/bin"
-# export PATH="$PATH:/usr/local/sbin"
-# export PATH="$PATH:$HOME/.cabal/bin"
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+export PATH="/usr/local/bin:$PATH"
+export PATH="$PATH:$HOME/bin"
+export PATH="$PATH:/usr/local/sbin"
+export PATH="$PATH:$HOME/.cabal/bin"
 # # export PATH="/usr/local/bin:/Users/danubilla/.rvm/gems/ruby-2.0.0-p353@viewthespace/bin:/Users/danubilla/.rvm/gems/ruby-2.0.0-p353@global/bin:/Users/danubilla/.rvm/rubies/ruby-2.0.0-p353/bin:/Users/danubilla/.rvm/bin:/bin:/usr/sbin:/sbin:/usr/bin:$PATH"
 # # export MANPATH="/usr/local/man:$MANPATH"
-# export PATH="/usr/local/bin:$PATH"
+export PATH="/usr/local/bin:$PATH"
