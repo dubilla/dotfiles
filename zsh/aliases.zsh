2d9c7c4597c4b1882eaa740f97ebb30c3591573c
Dan Ubilla <dan.ubilla@gmail.com>
2015-12-02 16:30:09 -0500

    Update latest project aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,8 +1,9 @@
 alias reload!='. ~/.zshrc'
 
 # Projects
-alias vts="cd ~/workspace/viewthespace"
+alias vts="cd ~/workspace/vts"
 alias golf="cd ~/Projects/Augusta"
+alias cfb="cd ~/Projects/CFBPickem"
 
 # Doge Git
 alias such=git
